618d8f05211c272d4a0ad9aa8e10d76606c7b9e1
Abraham Bobosha <115538910+Abraham-Bobosha@users.noreply.github.com>
2022-12-06 06:18:46 +0100

    Initial commit

---
+++ src/pages/Sara.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Sara() {
+  return (
+    <>
+      <h1>Sara</h1>
+      <p>
+        Aliquam iaculis a nisi sed ornare. Sed venenatis tellus vel consequat
+        congue. In bibendum vestibulum orci et feugiat.
+      </p>
+    </>
+  )
+}
+
+export default Sara
